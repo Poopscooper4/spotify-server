ac9eeaa07588f29344c34ccd205b2b24b58367ee
Rati Elizbarashvili <elizbarashvilirati15@Gmail.com>
2025-09-09 20:20:58 +0400

    Revert "Add global error handling middleware to server"
    This reverts commit fa182aecb6c8f28a4937cdf68bca767284c8a8f0.

--- src/server.js
+++ src/server.js
@@ -2,7 +2,6 @@ const express = require("express");
 const dotenv = require("dotenv");
 const userRouter = require("./routes/userRoutes");
 const connectedDB = require("./config/dbConnect");
-const { StatusCodes } = require("http-status-codes");
 
 //load env variables
 
@@ -16,27 +15,11 @@ const PORT = process.env.PORT || 5000;
 //Middleware to parse JSON requests
 app.use(express.json());
 
-//Routes
+//Routes  
 
+
+// middleware for user routes
 app.use("/api/users", userRouter);
-
-//  ERROR HANDLING
-
-app.use((req, res, next) => {
-  const error = new Error("not foundd - " + req.originalUrl);
-
-  res.status = StatusCodes.NOT_FOUND;
-  next(error);
-});
-
-// global error handler
-
-app.use((err, req, res, next) => {
-  res.status(err.status || StatusCodes.INTERNAL_SERVER_ERROR).json({
-    message: err.message || "internal server error",
-    status: "error",
-  });
-});
 
 const startServer = async () => {
   await connectedDB();
